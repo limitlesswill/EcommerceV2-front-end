78eeb2b737df1ce3284f971a3738b44f2ae93f6b
AhmedMakram <a7medd98@gmail.com>
2024-03-18 15:08:04 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-project',
+  // template:`<h1>Test</h1>`,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ECommerceUI';
+  // data binding => Interpolation {{title}}
+  title:string = 'First Project';
+  numberOne : number = 10;
+  numbertwo : number = 20;
+
+  add()
+  {
+    return this.numberOne + this.numbertwo;
+  }
+
+  SayHello(name:string)
+  {
+    return "Hello "+name;
+  }
+
 }
